d593cd2df609915ffdddbe10fd9c0a57926cd0a2
ANSH-creator-ui <singhnegiansh@gmail.com>
2026-09-12 23:16:07 +0530

    chore: initialize PageCache project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "=================================\n";
+    std::cout << "          PAGECACHE\n";
+    std::cout << "Performance Experimentation Platform\n";
+    std::cout << "Version 0.1\n";
+    std::cout << "=================================\n";
+
+    return 0;
+}
