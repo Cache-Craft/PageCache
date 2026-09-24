18150611bcced380d170a7f3a91856feb30d026d
Yatharth <yatharththapliyal405@gmail.com>
2026-09-24 23:36:35 +0530

    git commit -m "Add locality workload generation"

--- cpp-engine/workload/WorkloadGenerator.cpp
+++ cpp-engine/workload/WorkloadGenerator.cpp
@@ -29,3 +29,20 @@ vector<int> WorkloadGenerator::randomWorkload(int n, int u)
 
     return p;
 }
+    vector<int> WorkloadGenerator::locality(int n, int u)
+   {
+    mt19937 g(42);
+    uniform_int_distribution<int> all(0, u - 1);
+
+    int hot = max(1, u / 5);
+
+    uniform_int_distribution<int> h(0, hot - 1);
+    uniform_real_distribution<double> x(0, 1);
+
+    vector<int> p;
+
+    for (int i = 0; i < n; i++)
+        p.push_back(x(g) < 0.8 ? h(g) : all(g));
+
+    return p;
+}
