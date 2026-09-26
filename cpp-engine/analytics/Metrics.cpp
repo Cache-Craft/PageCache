fe6092463297021399157f2f7b934be3f5ff60a5
Yatharth <yatharththapliyal405@gmail.com>
2026-09-26 19:52:02 +0530

    Add metrics module structure

---
+++ cpp-engine/analytics/Metrics.cpp
@@ -0,0 +1,10 @@
+#include "Metrics.h"
+
+using namespace std;
+
+// Metrics are calculated inside CacheManager in this version.
+// This translation unit exists so the analytics module is independently extensible.
+
+void Metrics::calculate(Result&)
+{
+}
---
+++ cpp-engine/analytics/Metrics.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../cache/CacheManager.h"
+
+using namespace std;
+
+class Metrics
+{
+public:
+    static void calculate(Result&);
+};
